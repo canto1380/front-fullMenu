9c77980e05b05c07fe0c1e6f6807f7381bdf23c6
Alejandro Poviña <alejandro.povina@gmail.com>
2021-07-19 23:49:54 -0300

    se agrego dbJSON, se creo creo MenuCategorias para admin con logica de agregar y eliminar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,36 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import AdminPrincipal from './components/menu-admin/AdminPrincipal';
 import ClientePrincipal from './components/menu-cliente/ClientePrincipal';
 import SidebarCliente from './components/SideBarCliente'
+import AdminMenuCategoria from './components/menu-admin/AdminMenuCategoria';
 
 function App() {
+
+  //Categorias
+  const [categorias, setCategorias] = useState([]);
+  const [consultarCat, setConsultarCat]= useState(true);
+
+  const consultarAPICat = async() =>{
+    try{
+      const res = await fetch(
+        process.env.REACT_APP_API_URL + "/categorias"
+      );
+      const infoCategorias = await res.json();
+      if(res.status===200){
+        setCategorias(infoCategorias);
+        setConsultarCat(false);
+      }
+    }
+    catch(error){
+        console.log(error)
+      }
+    };
+
+  useEffect(()=>{
+    if(consultarCat){
+      consultarAPICat();
+    } 
+  },[consultarCat]);
+
   return (
     <Router>
       <Switch>
@@ -16,8 +44,9 @@ function App() {
         <Route exact path='/cliente'>
           <ClientePrincipal></ClientePrincipal>
         </Route>
-        <Route exact path='/a'>
+        <Route exact path='/adminMenu/categorias'>
           <SidebarCliente/>
+          <AdminMenuCategoria consultarCat={consultarCat} setConsultarCat={setConsultarCat} categorias={categorias}/>
         </Route>
       </Switch>
     </Router>
